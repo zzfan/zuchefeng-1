06cddac56fa918ebb96d68dff58b97c1e7e197ff
Hebi Li <lihebi@mail.ustc.edu.cn>
2014-04-20 20:29:38 +0800

    removed all strategies for social account

--- config/passport.js
+++ config/passport.js
@@ -1,10 +1,5 @@
 var mongoose = require('mongoose')
   , LocalStrategy = require('passport-local').Strategy
-  , TwitterStrategy = require('passport-twitter').Strategy
-  , FacebookStrategy = require('passport-facebook').Strategy
-  , GitHubStrategy = require('passport-github').Strategy
-  , GoogleStrategy = require('passport-google-oauth').OAuth2Strategy
-  , LinkedinStrategy = require('passport-linkedin').Strategy
   , User = mongoose.model('User')
   , Seller = mongoose.model('Seller')
 
@@ -14,13 +9,15 @@ module.exports = function (passport, config) {
 
   // serialize sessions
   passport.serializeUser(function(user, done) {
+    // here comes the obj in the following method, deserializeUser
     done(null, {id: user.id, type: user.type})
   })
 
   passport.deserializeUser(function(obj, done) {
 	  if (obj.type == 'Seller') {
 	      Seller.findOne({ _id: obj.id }, function (err, user) {
-	        done(err, user)
+	        // this user is used in req.user?
+          done(err, user)
 	      })
 	  }
 	  else {
@@ -29,7 +26,8 @@ module.exports = function (passport, config) {
 	      })
 	  }
   })
-  
+
+  // seller strategy
   passport.use('seller', new LocalStrategy({
 	  usernameField: 'email',
 	  passwordField: 'password'
@@ -43,6 +41,7 @@ module.exports = function (passport, config) {
 			if (!seller.authenticate(password)) {
 				return done(null, false, { message: 'Invalid password' })
 			}
+      // here comes the seller, used by serializeUser
 			return done(null, seller);
 		})
 	}
@@ -66,143 +65,4 @@ module.exports = function (passport, config) {
       })
     }
   ))
-/*
-  // use twitter strategy
-  passport.use(new TwitterStrategy({
-      consumerKey: config.twitter.clientID,
-      consumerSecret: config.twitter.clientSecret,
-      callbackURL: config.twitter.callbackURL
-    },
-    function(token, tokenSecret, profile, done) {
-      User.findOne({ 'twitter.id_str': profile.id }, function (err, user) {
-        if (err) { return done(err) }
-        if (!user) {
-          user = new User({
-            name: profile.displayName,
-            username: profile.username,
-            provider: 'twitter',
-            twitter: profile._json
-          })
-          user.save(function (err) {
-            if (err) console.log(err)
-            return done(err, user)
-          })
-        }
-        else {
-          return done(err, user)
-        }
-      })
-    }
-  ))
-
-  // use facebook strategy
-  passport.use(new FacebookStrategy({
-      clientID: config.facebook.clientID,
-      clientSecret: config.facebook.clientSecret,
-      callbackURL: config.facebook.callbackURL
-    },
-    function(accessToken, refreshToken, profile, done) {
-      User.findOne({ 'facebook.id': profile.id }, function (err, user) {
-        if (err) { return done(err) }
-        if (!user) {
-          user = new User({
-            name: profile.displayName,
-            email: profile.emails[0].value,
-            username: profile.username,
-            provider: 'facebook',
-            facebook: profile._json
-          })
-          user.save(function (err) {
-            if (err) console.log(err)
-            return done(err, user)
-          })
-        }
-        else {
-          return done(err, user)
-        }
-      })
-    }
-  ))
-
-  // use github strategy
-  passport.use(new GitHubStrategy({
-      clientID: config.github.clientID,
-      clientSecret: config.github.clientSecret,
-      callbackURL: config.github.callbackURL
-    },
-    function(accessToken, refreshToken, profile, done) {
-      User.findOne({ 'github.id': profile.id }, function (err, user) {
-        if (!user) {
-          user = new User({
-            name: profile.displayName,
-            email: profile.emails[0].value,
-            username: profile.username,
-            provider: 'github',
-            github: profile._json
-          })
-          user.save(function (err) {
-            if (err) console.log(err)
-            return done(err, user)
-          })
-        } else {
-          return done(err, user)
-        }
-      })
-    }
-  ))
-
-  // use google strategy
-  passport.use(new GoogleStrategy({
-      clientID: config.google.clientID,
-      clientSecret: config.google.clientSecret,
-      callbackURL: config.google.callbackURL
-    },
-    function(accessToken, refreshToken, profile, done) {
-      User.findOne({ 'google.id': profile.id }, function (err, user) {
-        if (!user) {
-          user = new User({
-            name: profile.displayName,
-            email: profile.emails[0].value,
-            username: profile.username,
-            provider: 'google',
-            google: profile._json
-          })
-          user.save(function (err) {
-            if (err) console.log(err)
-            return done(err, user)
-          })
-        } else {
-          return done(err, user)
-        }
-      })
-    }
-  ));
-
-  // use linkedin strategy
-  passport.use(new LinkedinStrategy({
-    consumerKey: config.linkedin.clientID,
-    consumerSecret: config.linkedin.clientSecret,
-    callbackURL: config.linkedin.callbackURL,
-    profileFields: ['id', 'first-name', 'last-name', 'email-address']
-    },
-    function(accessToken, refreshToken, profile, done) {
-      User.findOne({ 'linkedin.id': profile.id }, function (err, user) {
-        if (!user) {
-          user = new User({
-            name: profile.displayName
-          , email: profile.emails[0].value
-          , username: profile.emails[0].value
-          , provider: 'linkedin'
-          })
-          user.save(function (err) {
-            if (err) console.log(err)
-            return done(err, user)
-          })
-        } else {
-          return done(err, user)
-        }
-      })
-    }
-    ));
-    */
 }
